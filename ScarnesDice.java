f99f37a0fea2520676074ee63c409bc33f1476ee
sunilpodilapu <sunil.p.1729@gmail.com>
2016-12-29 23:27:11 +0530

    Update ScarnesDice.java

--- ScarnesDice.java
+++ ScarnesDice.java
@@ -1,17 +1,6 @@
 import java.util.*;
 class ScarnesDice
 {
-   System.out.println("\t\t\t-----------SCARNES DICE------------------");
-    System.out.println("\n\n");
-    System.out.println("\t\t\t------------INSTRUCTIONS-----------------");
-    System.out.println("1: This is a two player game");
-    System.out.println("2: First player starts the game");
-    System.out.println("3: The player who gets more than or equal to 100 wins the game");
-    System.out.println("4: In one turn the dice is rolled and player has two choices");
-    System.out.println("\t4.1: one he can hold his score and add this to total score");
-    System.out.println("\t4.2:He can try another chance by repetedly throw dice and increase his score");
-    System.out.println("5: If he gets one he is fired,his score in that turn becomes zero");
-    System.out.println("\t\t\t-------------------------------GOOD LUCK---------------------------");
    static int whose_move=1;
    static int ply1=0,ply2=0;
    static int limit=100;
@@ -22,6 +11,17 @@ class ScarnesDice
    static int who_win=0;
    public static void main(String s[])
    {
+    System.out.println("\t\t\t-----------SCARNES DICE------------------");
+    System.out.println("\n\n");
+    System.out.println("\t\t\t------------INSTRUCTIONS-----------------");
+    System.out.println("1: This is a two player game");
+    System.out.println("2: First player starts the game");
+    System.out.println("3: The player who gets more than or equal to 100 wins the game");
+    System.out.println("4: In one turn the dice is rolled and player has two choices");
+    System.out.println("\t4.1: one he can hold his score and add this to total score");
+    System.out.println("\t4.2:He can try another chance by repetedly throw dice and increase his score");
+    System.out.println("5: If he gets one he is fired,his score in that turn becomes zero");
+    System.out.println("\t\t\t-------------------------------GOOD LUCK---------------------------");
        Random pl1=new Random();
        Random pl2=new Random();
        System.out.println("Player 1 starts");
@@ -42,6 +42,7 @@ class ScarnesDice
        {
         if(p==1)
         {
+            ply1=0;
           pl1_curturnscore=0;
             whose_move=2;
             System.out.println("you  got 1");
@@ -55,7 +56,7 @@ class ScarnesDice
        c=sc.next().charAt(0);
          if(c=='y')
           {
-             ply1=ply1+p;     
+             ply1=ply1+p; 
            whose_move=1;
           }
          else
@@ -75,6 +76,7 @@ class ScarnesDice
         {
          if(p==1)
          {
+             ply2=0;
                   System.out.println("you got 1");
            System.out.println("player 1 turn");
            pl1_curturnscore=0;
